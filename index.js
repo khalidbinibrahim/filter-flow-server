f0237aeaf7b6d3e566d6d82e975c86d858959a82
khalidbinibrahim <khalidbinibrahim01@gmail.com>
2024-08-16 17:52:15 +0600

    added vercel publishment

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 const uri = process.env.MONGO_URI;
@@ -22,12 +27,12 @@ let db;
 
 async function connectDb() {
   try {
-    await client.connect();
+    // await client.connect();
     db = client.db('filterFlowDB');
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
-    process.exit(1);
+    // process.exit(1);
   }
 }
 
@@ -37,28 +42,28 @@ app.get('/products', async (req, res) => {
   const { page = 1, limit = 10, search = '', sortBy = 'creationDate', order = 'desc', category, brand, minPrice, maxPrice } = req.query;
 
   let query = {
-      name: { $regex: search, $options: 'i' },
-      ...(category && { category }),
-      ...(brand && { brand }),
-      ...(minPrice && maxPrice && { price: { $gte: parseFloat(minPrice), $lte: parseFloat(maxPrice) } }),
+    name: { $regex: search, $options: 'i' },
+    ...(category && { category }),
+    ...(brand && { brand }),
+    ...(minPrice && maxPrice && { price: { $gte: parseFloat(minPrice), $lte: parseFloat(maxPrice) } }),
   };
 
   let sort = { [sortBy]: order === 'desc' ? -1 : 1 };
 
   try {
-      const products = await db.collection('products')
-          .find(query)
-          .sort(sort)
-          .skip((page - 1) * limit)
-          .limit(parseInt(limit))
-          .toArray();
+    const products = await db.collection('products')
+      .find(query)
+      .sort(sort)
+      .skip((page - 1) * limit)
+      .limit(parseInt(limit))
+      .toArray();
 
-      const total = await db.collection('products').countDocuments(query);
+    const total = await db.collection('products').countDocuments(query);
 
-      res.json({ products, totalPages: Math.ceil(total / limit) });
+    res.json({ products, totalPages: Math.ceil(total / limit) });
   } catch (err) {
-      console.error('Failed to fetch products', err);
-      res.status(500).json({ error: 'Failed to fetch products' });
+    console.error('Failed to fetch products', err);
+    res.status(500).json({ error: 'Failed to fetch products' });
   }
 });
 
